3623c0767eac23977ac284c13bdead2811ccae9c
Athavan Kanapuli <athavankanapuli@gmail.com>
2023-12-27 21:15:20 +0100

    ADD Http handler for Put operation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kanapuli/pinnacle
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 var store map[string]string
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	_, _ = w.Write([]byte("Success"))
+}
+
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	v := mux.Vars(r)
+	key := v["key"]
+	value, err := io.ReadAll(r.Body)
+
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
 func main() {
 	store = make(map[string]string)
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	log.Println("Listening on port 8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 func Put(key, value string) error {

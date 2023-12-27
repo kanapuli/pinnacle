8767f781c343970d83ac1d2a0d67946ec07261ea
Athavan Kanapuli <athavankanapuli@gmail.com>
2023-12-27 19:31:18 +0100

    Initial Commit: ADD store map and basic PUT, GET functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kanapuli/pinnacle
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "errors"
+
+var store map[string]string
+
+func main() {
+	store = make(map[string]string)
+}
+
+func Put(key, value string) error {
+	// make the put operation idempotent
+	store[key] = value
+	return nil
+}
+
+var (
+	ErrKeyNotFound = errors.New("key not found")
+)
+
+func Get(key string) (string, error) {
+	if v, ok := store[key]; ok {
+		return v, nil
+	}
+	return "", ErrKeyNotFound
+}
